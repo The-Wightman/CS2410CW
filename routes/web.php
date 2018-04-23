f196739ee3f3547f2606c7414c99d8788542b8b9
The-Woghtman <david199619@live.co.uk>
2018-04-23 13:28:51 +0100

    deploy version1 Added comments V1

--- routes/web.php
+++ routes/web.php
@@ -11,45 +11,57 @@
 |
 */
 
-Route::get('/', function () {
-    return redirect()->route('home');
-});
+//Route all connections to / through a redirect function taking them to the home Route
+Route::get('/', function () {return redirect()->route('home');});
 
-
-
-
+//Return the the url extension list with the content created by the EventController@list command, and name this route list to allow easier calling.
 Route::get('list', 'EventController@list')->name('list');
 
+//Return the url extension show/{ID} with the content created by the EventController@show command, and name this route show to allow easier calling. The ID value is called from the request passed with the Route call.
 Route::get('show/{id}', 'EventController@show')->name('show');
 
-Route::get('display','EventController@display')->name('display_event');
-
+//Return the the url extension UserList with the content created by the UserController@list command, and name this route userlist to allow easier calling.
 Route::get('UserList','UserController@list')->name('userlist');
 
+//Return the the url extension UserProfile/{ID} with the content created by the UserController@profile command, and name this route userprofile to allow easier calling. The ID value is called from the request passed with the Route call.
 Route::get('Userprofile/{id}','UserController@profile')->name('userprofile');
 
+//Return the the url extension Userevents with the content created by the UserController@myevents command, and name this route userevents to allow easier calling.
 Route::get('Userevents','EventController@myevents')->name('userevents');
 
+//Return the url extension showcategory/{category} with the content created by the EventController@showbytype command, and name this route category to allow easier calling. The category value is called from the enum value passed with the Route call.
 Route::get('showcategory/{category}', 'EventController@showbytype')->name('category');
 
-Auth::routes();
-
-Route::post('eventsort','EventController@sort')->name('eventsort');
-
-Route::post('usersort','UserController@sort')->name('usersort');
-
+//Return the the url extension home with the content created by the HomeController@index command, and name this route home to allow easier calling.
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::middleware('auth')->get('updateevent/{id}', 'EventController@alterevent')->name('alterevent');
+//This sets up the routes for the login and authentication pages login and register
+Auth::routes();
 
-Route::middleware('auth')->post('updateevent/{id}' , 'EventController@updateevent')->name('updateevent');
+// Take the inputted extension eventsort and pass it and the request to the EventController@sort. The route is named eventsort for easier calling.
+Route::post('eventsort','EventController@sort')->name('eventsort');
 
-Route::middleware('auth')->post('deleteevent/{id}' , 'EventController@deleteevent')->name('deleteevent');
+// Take the inputted extension usersort and pass it and the request to the UserController@sort. The route is named usersort for easier calling.
+Route::post('usersort','UserController@sort')->name('usersort');
 
-Route::middleware('auth')->get('createevent','EventController@createevent')->name('createevent');
-
-Route::middleware('auth')->post('createevent','EventController@create')->name('create.event');
-
+// Take the inputted extension like/{ID} and pass it and the request to the EventController@like. The route is named likevent for easier calling and the ID value is pulled from the request.
 Route::post('like/{id}', 'EventController@like')->name('likeevent');
 
+// Take the inputted extension unlike/{ID} and pass it and the request to the EventController@unlike. The route is named unlikevent for easier calling and the ID value is pulled from the request.
 Route::post('unlike/{id}', 'EventController@unlike')->name('unlikeevent');
+
+//Check if the User making the request is an authorised user. If they arent then reroute to the login screen. If they are return the the url extension updateevent{ID} with the content created by the EventController@alterevent command, and name this alterevent to allow easier calling.
+Route::middleware('auth')->get('updateevent/{id}', 'EventController@alterevent')->name('alterevent');
+
+//Check if the User making the request is an authorised user. If they arent then reroute to the login screen. If they are return the the url extension createevent with the content created by the EventController@createevent command, and name this createevent to allow easier calling.
+Route::middleware('auth')->get('createevent','EventController@createevent')->name('createevent');
+
+//Check if the User making the request is an authorised user. If they arent then reroute to the login screen. If they are take the inputted extension updateevent/{id} and pass it and the request to the EventController@updateevent. The route is named updateevent for easier calling and the ID value is pulled from the request.
+Route::middleware('auth')->post('updateevent/{id}' , 'EventController@updateevent')->name('updateevent');
+
+//Check if the User making the request is an authorised user. If they arent then reroute to the login screen. If they are take the inputted extension deleteevent/{id} and pass it and the request to the EventController@deleteevent. The route is named deleteevent for easier calling and the ID value is pulled from the request.
+Route::middleware('auth')->post('deleteevent/{id}' , 'EventController@deleteevent')->name('deleteevent');
+
+//Check if the User making the request is an authorised user. If they arent then reroute to the login screen. If they are take the inputted extension createevent/{id} and pass it and the request to the EventController@create. The route is named create.event for easier calling.
+Route::middleware('auth')->post('createevent','EventController@create')->name('create.event');
+
